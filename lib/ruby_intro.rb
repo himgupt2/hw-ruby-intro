9faf08647ac297791690c0d50ab98f58ef74ed25
Himanshu Gupta <himgupt2@gmail.com>
2018-02-13 23:25:49 +0000

    HW1 Commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,67 @@
 
 def sum arr
   # YOUR CODE HERE
+  s = 0
+  arr.each do |x|
+    s+= x
+  end
+
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  else
+    arr.sort!.reverse!
+    return arr[0] + arr [1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).to_a.each do |x|
+    if sum(x) == n
+      return true
+    end
+  end
+  
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /[bcdfghjklmnprstvwxyz]/i =~ s[0]
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s=="0"
+    return true
+  elsif s.to_i(2) > 0
+    return true if s.to_i(2) % 4 == 0
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+def initialize(n, p)
+  raise ArgumentError if n.empty? or p<=0
+  @isbn,@price= n,p
+end
+
+def price_as_string
+  "$%.2f" % @price
+end
+
 end
